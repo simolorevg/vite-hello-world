b3b957f665c42ae3b4c30b1f6ffaf54ef24355ad
simolorevg <simoneviaggi@libero.it>
2023-04-12 15:29:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import bannerTitle from './components/AppBanner.vue';
+export default {
+  components: {
+    bannerTitle
+  },
+  data() {
+    return {
+      title: 'Hello Vite'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <bannerTitle />
+</template>
+
+<style></style>
---
+++ src/components/AppBanner.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    data() {
+        return {
+            bannerTitle: 'This is AppBanner'
+        }
+    }
+}
+</script>
+<template>
+    <h2>{{ bannerTitle }}</h2>
+</template>
+<style></style>
